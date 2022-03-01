ae7f37c1faa87b04d3ec944a4605a15a3842a40d
AlerHugu3s <axiasiming@gmail.com>
2022-03-01 16:48:04 +0800

    revert

--- TheOtherRoles/Modules/CustomHats.cs
+++ TheOtherRoles/Modules/CustomHats.cs
@@ -366,22 +366,18 @@ namespace TheOtherRoles.Modules {
 
     public class CustomHatLoader {
         public static bool running = false;
-        private const string REPO = "https://gitee.com/KarasumaChitose/TheOtherHats/raw/master";
 
         public static List<CustomHatOnline> hatdetails = new List<CustomHatOnline>();
-        private static Task hatFetchTask = null;
         public static void LaunchHatFetcher() {
             if (running)
                 return;
             running = true;
-            hatFetchTask = LaunchHatFetcherAsync();
+            LaunchHatFetcherAsync();
         }
 
-        private static async Task LaunchHatFetcherAsync() {
+        private static void LaunchHatFetcherAsync() {
             try {
-                HttpStatusCode status = await FetchHats();
-                if (status != HttpStatusCode.OK)
-                    System.Console.WriteLine("Custom Hats could not be loaded\n");
+                FetchHats();
             } catch (System.Exception e) {
                 System.Console.WriteLine("Unable to fetch hats\n" + e.Message);
             }
@@ -399,21 +395,15 @@ namespace TheOtherRoles.Modules {
             return res;
         }
 
-        public static async Task<HttpStatusCode> FetchHats() {
+        public static void FetchHats() {
             HttpClient http = new HttpClient();
             http.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue{ NoCache = true };
-            var response = await http.GetAsync(new System.Uri($"{REPO}/CustomHats.json"), HttpCompletionOption.ResponseContentRead).ConfigureAwait(false);
             try {
-                if (response.StatusCode != HttpStatusCode.OK) return response.StatusCode;
-                if (response.Content == null) {
-                    System.Console.WriteLine("Server returned no data: " + response.StatusCode.ToString());
-                    return HttpStatusCode.ExpectationFailed;
-                }
-                string json = await response.Content.ReadAsStringAsync();
-                TheOtherRolesPlugin.Logger.LogMessage(json);
+                string jsonPath = Path.GetDirectoryName(Application.dataPath) + @"\CustomHats.json";
+                string json = File.ReadAllText(jsonPath);
+                
                 JToken jobj = JObject.Parse(json)["hats"];
-                if (!jobj.HasValues) return HttpStatusCode.ExpectationFailed;
-
+                
                 List<CustomHatOnline> hatdatas = new List<CustomHatOnline>();
 
                 for (JToken current = jobj.First; current != null; current = current.Next) {
@@ -447,39 +437,39 @@ namespace TheOtherRoles.Modules {
                 List<string> markedfordownload = new List<string>();
 
                 string filePath = Path.GetDirectoryName(Application.dataPath) + @"\TheOtherHats\";
+                
+                
+                
                 MD5 md5 = MD5.Create();
-                foreach (CustomHatOnline data in hatdatas) {
-    	            if (doesResourceRequireDownload(filePath + data.resource, data.reshasha, md5))
+                for (int i = 0; i < hatdatas.Count; i++)
+                {
+                    CustomHatOnline data = hatdatas[i];
+                    markedfordownload.Clear();
+                    
+                    if (doesResourceRequireDownload(filePath + data.resource, data.reshasha, md5))
                         markedfordownload.Add(data.resource);
-    	            if (data.backresource != null && doesResourceRequireDownload(filePath + data.backresource, data.reshashb, md5))
+                    if (data.backresource != null && doesResourceRequireDownload(filePath + data.backresource, data.reshashb, md5))
                         markedfordownload.Add(data.backresource);
-    	            if (data.climbresource != null && doesResourceRequireDownload(filePath + data.climbresource, data.reshashc, md5))
+                    if (data.climbresource != null && doesResourceRequireDownload(filePath + data.climbresource, data.reshashc, md5))
                         markedfordownload.Add(data.climbresource);
-    	            if (data.flipresource != null && doesResourceRequireDownload(filePath + data.flipresource, data.reshashf, md5))
+                    if (data.flipresource != null && doesResourceRequireDownload(filePath + data.flipresource, data.reshashf, md5))
                         markedfordownload.Add(data.flipresource);
-    	            if (data.backflipresource != null && doesResourceRequireDownload(filePath + data.backflipresource, data.reshashbf, md5))
+                    if (data.backflipresource != null && doesResourceRequireDownload(filePath + data.backflipresource, data.reshashbf, md5))
                         markedfordownload.Add(data.backflipresource);
+                    
+                    if (markedfordownload.Count != 0)
+                        hatdatas.RemoveAt(i);
                 }
                 
-                foreach(var file in markedfordownload) {
-                    
-                    var hatFileResponse = await http.GetAsync($"{REPO}/hats/{file}", HttpCompletionOption.ResponseContentRead);
-                    if (hatFileResponse.StatusCode != HttpStatusCode.OK) continue;
-                    using (var responseStream = await hatFileResponse.Content.ReadAsStreamAsync()) {
-                        using (var fileStream = File.Create($"{filePath}\\{file}")) {
-                            responseStream.CopyTo(fileStream);
-                        }
-                    }
-                }
-
                 hatdetails = hatdatas;
             } catch (System.Exception ex) {
                 TheOtherRolesPlugin.Instance.Log.LogError(ex.ToString());
                 System.Console.WriteLine(ex);
             }
-            return HttpStatusCode.OK;
         }
 
+        
+        
         private static bool doesResourceRequireDownload(string respath, string reshash, MD5 md5) {
             if (reshash == null || !File.Exists(respath)) 
                 return true;
