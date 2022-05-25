8557327ed2c5f17e4c55477a6d0ee6223c06330f
MapleEve <esanisa@gmail.com>
2022-05-26 02:16:49 +0800

    中文标点

--- TheOtherRoles/Patches/CredentialsPatch.cs
+++ TheOtherRoles/Patches/CredentialsPatch.cs
@@ -55,7 +55,7 @@ $@"<size=60%> <color=#FCCE03FF>感谢 K3ndo & Smeggy 感谢 四个憨批汉化�
                 __instance.text.alignment = TMPro.TextAlignmentOptions.TopRight;
                 if (AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started) {
                     __instance.text.text = $"<size=130%><color=#ff351f>超多职业</color></size> v{TheOtherRolesPlugin.Version.ToString()}\n<color=#1a75ff>兰博玩对战(内测)本地帽子</color>\n" \n" +
-                $"延迟: {AmongUsClient.Instance.Ping}毫秒\n" +
+                $"延迟：{AmongUsClient.Instance.Ping}毫秒\n" +
                     if (CachedPlayer.LocalPlayer.Data.IsDead || (!(CachedPlayer.LocalPlayer.PlayerControl == null) && (CachedPlayer.LocalPlayer.PlayerControl == Lovers.lover1 || CachedPlayer.LocalPlayer.PlayerControl == Lovers.lover2))) {
                         __instance.transform.localPosition = new Vector3(3.45f, __instance.transform.localPosition.y, __instance.transform.localPosition.z);
                     } else {
